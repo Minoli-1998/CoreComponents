42e8bac196c0cd6c6e462b8fd3b14bbe9146518b
Minoli <mbhagyagunathilake@gmail.com>
2023-10-15 10:35:52 +0530

    Learnt about Pressable component

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { View, Button } from "react-native";
+import { View, Button, Image, Text, Pressable } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
@@ -7,8 +8,25 @@ export default function App() {
         title="Press"
         onPress={() => console.log("Button pressed")}
         color="midnightblue"
-        disabled
       />
+      <Pressable onPress={() => console.log("Image pressed")}>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </Pressable>
+      <Pressable onPress={() => console.log("Text pressed")}>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+          recusandae officiis minus consequatur accusantium molestiae neque
+          quidem cum deserunt, earum assumenda perspiciatis omnis natus culpa
+          voluptas atque voluptate quam debitis. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Minima, harum assumenda. Cum fugit
+          aliquid, doloribus quam atque accusantium! A ipsa magni et sequi
+          laudantium culpa atque ut consectetur dolorem ea. Lorem ipsum dolor
+          sit amet, consectetur adipisicing elit. Distinctio delectus suscipit
+          itaque? Veniam beatae eligendi quod dignissimos quo maiores
+          voluptatibus atque quidem, consequuntur at perspiciatis tempore,
+          officiis, expedita voluptatum natus!
+        </Text>
+      </Pressable>
     </View>
   );
 }
