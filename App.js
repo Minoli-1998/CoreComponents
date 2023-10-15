96b59f7043b299a7f16ea2d253440aae27cbb09b
Minoli <mbhagyagunathilake@gmail.com>
2023-10-15 09:45:37 +0530

    Learn about scroll view

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
-import { View, Text, Image, ImageBackground } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      {/*<Image source={logoImg} style={{ width: 200, height: 200 }} />
-      <Image
-        source={{ uri: "https://picsum.photos/200" }}
-        style={{ width: 200, height: 200 }}
-  />*/}
-      <ImageBackground source={logoImg} style={{ flex: 1 }}>
-        <Text>IMAGE TEXT</Text>
-      </ImageBackground>
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+          officia et animi ullam possimus alias voluptates velit dolorem modi.
+          Id sunt illo itaque unde ex explicabo ipsam eum modi dolorum? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Molestias, delectus
+          enim at, voluptatum saepe suscipit mollitia reprehenderit culpa
+          ratione, obcaecati ea libero hic modi perferendis laboriosam rerum qui
+          corrupti. Inventore. Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Itaque labore dolores placeat, minima eligendi
+          molestias porro odio ad? Voluptatum facilis praesentium optio tempora
+          maxime voluptate amet dolore in, inventore minus!
+        </Text>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </ScrollView>
     </View>
   );
 }
