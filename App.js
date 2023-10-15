83df12487f291568eaacd1c8cc2e0d3198e672dc
Minoli <mbhagyagunathilake@gmail.com>
2023-10-15 19:49:48 +0530

    changes added

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import { View, Text, StyleSheet } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={[styles.box, styles.lightblueBg]}>
+      <View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
         <Text style={{ borderRadius: 10, backgroundColor: "lightpink" }}>
           Lightblue Box
         </Text>
       </View>
-      <View style={[styles.box, styles.lightgreenBg]}>
+      <View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
         <Text>Lightgreen Box</Text>
       </View>
     </View>
@@ -21,8 +21,8 @@ const styles = StyleSheet.create({
     /*width: "25%",
     height: "25%",
     padding: 10,*/
-    width: 100,
-    height: 100,
+    width: 250,
+    height: 250,
     paddingHorizontal: 10,
     paddingVertical: 20,
     marginVertical: 10,
@@ -35,5 +35,18 @@ const styles = StyleSheet.create({
   },
   lightgreenBg: {
     backgroundColor: "lightgreen",
+  },
+  boxShadow: {
+    shadowColor: "#333333",
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 10,
+  },
+  androidShadow: {
+    elevation: 20,
+    shadowColor: "white",
   },
 });
