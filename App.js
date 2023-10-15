79704af9bf0da370a0591b5c8d27c1a1207b7777
Minoli <mbhagyagunathilake@gmail.com>
2023-10-15 10:16:04 +0530

    Leaarn about Button component

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
-import { View, Text, Image, ScrollView } from "react-native";
-const logoImg = require("./assets/adaptive-icon.png");
+import { View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <ScrollView>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-          officia et animi ullam possimus alias voluptates velit dolorem modi.
-          Id sunt illo itaque unde ex explicabo ipsam eum modi dolorum? Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Molestias, delectus
-          enim at, voluptatum saepe suscipit mollitia reprehenderit culpa
-          ratione, obcaecati ea libero hic modi perferendis laboriosam rerum qui
-          corrupti. Inventore. Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Itaque labore dolores placeat, minima eligendi
-          molestias porro odio ad? Voluptatum facilis praesentium optio tempora
-          maxime voluptate amet dolore in, inventore minus!
-        </Text>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      </ScrollView>
+      <Button
+        title="Press"
+        onPress={() => console.log("Button pressed")}
+        color="midnightblue"
+        disabled
+      />
     </View>
   );
 }
