d20558a6f51892e52d940193d6b30c6dd0a496ee
Minoli <mbhagyagunathilake@gmail.com>
2023-10-15 12:26:46 +0530

    Learnt Modal component

--- App.js
+++ App.js
@@ -1,32 +1,30 @@
-import { View, Button, Image, Text, Pressable } from "react-native";
-const logoImg = require("./assets/adaptive-icon.png");
+import { useState } from "react";
+import { View, Button, Text, Modal } from "react-native";
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
       <Button
         title="Press"
-        onPress={() => console.log("Button pressed")}
+        onPress={() => setIsModalVisible(true)}
         color="midnightblue"
       />
-      <Pressable onPress={() => console.log("Image pressed")}>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      </Pressable>
-      <Pressable onPress={() => console.log("Text pressed")}>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          recusandae officiis minus consequatur accusantium molestiae neque
-          quidem cum deserunt, earum assumenda perspiciatis omnis natus culpa
-          voluptas atque voluptate quam debitis. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Minima, harum assumenda. Cum fugit
-          aliquid, doloribus quam atque accusantium! A ipsa magni et sequi
-          laudantium culpa atque ut consectetur dolorem ea. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit. Distinctio delectus suscipit
-          itaque? Veniam beatae eligendi quod dignissimos quo maiores
-          voluptatibus atque quidem, consequuntur at perspiciatis tempore,
-          officiis, expedita voluptatum natus!
-        </Text>
-      </Pressable>
+      <Modal
+        visible={isModalVisible}
+        animationType="slide"
+        presentationStyle="pageSheet"
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Text>Modal Content</Text>
+          <Button
+            title="Close"
+            color="midnightblue"
+            onPress={() => setIsModalVisible(false)}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
